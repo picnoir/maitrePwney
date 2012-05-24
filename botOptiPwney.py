1578f57527931a6c59453b404d9b4ab73c3def83
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2012-05-24 22:00:11 +0200

    Adding a config file system.

--- botOptiPwney.py
+++ botOptiPwney.py
@@ -1,22 +1,55 @@
 # -*- coding: utf-8 -*-
 
-import irclib
-import ircbot
+
+#                   ,%%%,
+#                 ,%%%` %
+#                ,%%`( '|             ..:[ OptiPwney ]:..
+#               ,%%@ /\_/
+#     ,%.-"""--%%% "@@__              Teh Pau HackerSpace
+#    %%/             |__`\                    
+#   .%'\     |   \   /  //             
+#   ,%' >   .'----\ |  [/               
+#      < <<`       ||      
+#       `\\\       ||                                   with pwnies !
+#         )\\      )\
+# ^^^^^^^^"""^^^^^^""^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+try: 
+    import irclib, ircbot, ConfigParser
+except:
+    print 'Missing dependancies, please install irclib, ircbot and ConfigParser'
+    system.exit(True)
+
 
 class BotPoney(ircbot.SingleServerIRCBot):
-    canal="#canal" """Canal sur lequel le bot doit se connecter"""
+"""Simple irc bot. Can set op mode to someone"""
+
+
     def __init__(self):
-        ircbot.SingleServerIRCBot.__init__(self, [("irc.freenode.net", 6665)],
-                                           "MaitrePwney", "Bot")
+        self.config = ConfigParser.RawConfigParser(allow_no_value=False)
+        self.config.read("maitrePwney.conf")
+        self.botPassword = self.config.get('config','password')
+        self.channel = self.config.get('config','channel')
+        self.serverAdress = self.config.get('config','adress')
+        self.serverPort = self.config.getint('config','port')
+        self.botName = self.config.get('config','name')
+        ircbot.SingleServerIRCBot.__init__(self, [(self.serverAdress, self.serverPort)],
+                                           self.botName, "Bot")
+
     def on_welcome(self, serv, ev):
-        serv.privmsg("nickserv", "identify motDePasse") """Identification"""
-        serv.join(self.canal)
+        serv.privmsg("nickserv", "identify {0}".format(self.botPassword))
+        serv.join(self.channel)
+
+
     def on_privmsg(self, serv, ev):
-        """Méthode appeée quand le bot reçoit un pm"""
-        demandeur = irclib.nm_to_n(ev.source())
-        if ev.arguments()[0]=="op" and demandeur in ["Utilisateurs","pouvant être op"]:
-            commande="+o {0}".format(demandeur)
-            serv.mode(self.canal,commande)
+        self.config.read("maitrePwney.conf")
+        usersAllowed = self.config.get('config','usersAllowed').strip().split(', ')
+        request = irclib.nm_to_n(ev.source())
+        if ev.arguments()[0]=="op" and request in usersAllowed:
+            query = "+o {0}".format(request)
+            serv.mode(self.channel,query)
+
 
 if __name__ == "__main__":
     BotPoney().start()
