9fd7aa832d918a8adc9830be6138ae2c88686d6b
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2012-05-24 23:02:52 +0200

    Adding poney ascii art

--- botOptiPwney.py
+++ botOptiPwney.py
@@ -23,7 +23,7 @@ except:
 
 
 class BotPoney(ircbot.SingleServerIRCBot):
-"""Simple irc bot. Can set op mode to someone"""
+    """Simple irc bot. Can set op mode to someone"""
 
 
     def __init__(self):
@@ -34,6 +34,7 @@ class BotPoney(ircbot.SingleServerIRCBot):
         self.serverAdress = self.config.get('config','adress')
         self.serverPort = self.config.getint('config','port')
         self.botName = self.config.get('config','name')
+        self.usersAllowed = self.config.get('config','usersAllowed').strip().split(', ')
         ircbot.SingleServerIRCBot.__init__(self, [(self.serverAdress, self.serverPort)],
                                            self.botName, "Bot")
 
@@ -44,11 +45,25 @@ class BotPoney(ircbot.SingleServerIRCBot):
 
     def on_privmsg(self, serv, ev):
         self.config.read("maitrePwney.conf")
-        usersAllowed = self.config.get('config','usersAllowed').strip().split(', ')
+        self.usersAllowed = self.config.get('config','usersAllowed').strip().split(', ')
         request = irclib.nm_to_n(ev.source())
-        if ev.arguments()[0]=="op" and request in usersAllowed:
+        if ev.arguments()[0]=="op" and request in self.usersAllowed:
             query = "+o {0}".format(request)
             serv.mode(self.channel,query)
+
+
+    def on_pubmsg(self, serv, ev):
+        request = irclib.nm_to_n(ev.source())
+        if ev.arguments()[0]=="dessine-moi un poney" and request in self.usersAllowed:
+            self.draw_poney(serv)
+            
+
+
+    def draw_poney(self, serv):
+        poneyAsciiArt = ['                   ,%%%,', '                 ,%%%` %', "                ,%%`( '|             ..:[ OptiPwney ]:..", '               ,%%@ /\\_/', '     ,%.-"""--%%% "@@__              Teh Pau HackerSpace', '    %%/             |__`\\                    ', "   .%'\\     |   \\   /  //             ", "   ,%' >   .'----\\ |  [/               ", '      < <<`       ||      ', '       `\\\\\\       ||                                   with pwnies !', '         )\\\\      )\\', ' ^^^^^^^^"""^^^^^^""^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', '']
+
+        for line in poneyAsciiArt:
+            serv.privmsg(self.channel, line)
 
 
 if __name__ == "__main__":
